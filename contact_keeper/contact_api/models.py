1cd9566e91511a1dab27d1e648d52deef1465b06
jideabdqudus <jideabdqudus@gmail.com>
2021-05-09 18:54:15 +0100

    API

--- contact_keeper/contact_api/models.py
+++ contact_keeper/contact_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=80, blank=True)
+    phone = models.IntegerField()
+    type = models.CharField(max_length=20)
+
+
